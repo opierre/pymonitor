0e2ef234041703248459c4cda3cf8e7f87dd31a2
opierre <pierre.o@hotmail.fr>
2026-04-04 11:59:33 +0200

    refactor(python): move monitor.py to pymonitor module

--- python/pymonitor/monitor.py
+++ python/pymonitor/monitor.py
@@ -1,4 +1,5 @@
 """A lightweight monitor for computer constants."""
+from src import _rust_monitor
 
 
 class PyMonitor:
@@ -19,7 +20,7 @@ class PyMonitor:
         # Store instance attributes
         self._db_url = database_url
         self._interval = interval
-        self._monitor_handle = None
+        self._monitor_handle: _rust_monitor.MonitorHandle | None = None
 
     def start(self) -> None:
         """Starts the background Rust monitoring thread.
@@ -36,4 +37,16 @@ class PyMonitor:
         if self._monitor_handle is not None:
             # TODO: stop the thread
             pass
+
+    @staticmethod
+    def get_process_mess_metrics(name: str) -> list[tuple[int, float, float]]:
+        """Retrieves resource usage for specific processes by name.
+
+        Args:
+            name: The exact name of the process.
+
+        Returns:
+            list[tuple[int, float, float]]: A list of tuples containing (pid, cpu_percent, ram_percent).
+        """
+        return _rust_monitor.get_process_metrics(name)
 
